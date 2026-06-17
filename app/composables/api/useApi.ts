3427ffc1822527b8a57fa4b384dbb411b95aadbb
webdragon <grmnche@yandex.ru>
2026-06-17 14:43:50 +0300

    изменени апи обертки

--- app/composables/api/useApi.ts
+++ app/composables/api/useApi.ts
@@ -1,15 +1,3 @@
 export const useAPI = createUseFetch({
-    baseURL: import.meta.env.VITE_BACKEND_PRODUCTION_URL,
-    onRequest ({ options }) {
-      const { session } = useUserSession()
-      if (session.value?.token) {
-        options.headers.set('Authorization', `Bearer ${session.value.token}`)
-      }
-    },
-    async onResponseError ({ response }) {
-      if (response.status === 401) {
-        await navigateTo('/login')
-      }
-    },
-  })
-  
+  baseURL: import.meta.env.VITE_BACKEND_PRODUCTION_URL
+})
